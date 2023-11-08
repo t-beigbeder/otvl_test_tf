b2c0e29cc2e5f3732ca736ec3b08a57c382de4fb
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-11-08 10:06:02 +0100

    mc

---
+++ README.md
@@ -0,0 +1,24 @@
+# Various tests with open tofu
+
+## Documentation
+
+- [Open Tofu documentation](https://opentofu.org/docs/cli/)
+- [Terraform](https://developer.hashicorp.com/terraform?product_intent=terraform)
+- [AWS Provider](https://registry.terraform.io/providers/hashicorp/aws/latest/docs)
+
+## Tool setup
+
+Avoid downloading 300 MB for aws plugin per tf launch
+
+    $ vi ~/.tofurc
+    plugin_cache_dir = "$HOME/.terraform.d/plugin-cache"
+
+## Help
+
+### General
+
+- [github terraform up and running](https://github.com/brikis98/terraform-up-and-running-code)
+- 
+### IAM
+
+- [Use AWS Managed Policies in Terraform](https://francescoboffa.com/terraform-aws-managed-policies/)
---
+++ t02_lr/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.1"
+    }
+  }
+}
+
+resource "aws_iam_service_linked_role" "aws_service_role_for_asg" {
+  aws_service_name = "autoscaling.amazonaws.com"
+  custom_suffix = "t02l"
+  description = "Allows EC2 Auto Scaling to use or manage AWS services and resources on your behalf."
+}
+
+data "aws_iam_policy" "administrator_access_policy" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role" "EC2AdminRole" {
+  name = "role-t02l-appname-dev-DevAdmin-ec2admin"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        ManagedPolicyArns = [
+          "${data.aws_iam_policy.administrator_access_policy.arn}"
+        ]
+      },
+    ]
+  })
+}
