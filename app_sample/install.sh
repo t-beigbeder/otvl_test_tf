e9d5bd4b8c0af2da6acd19290475d29bd4e1384a
thierry_otvl <tbeigbeder.nospam@gmail.com>
2023-11-29 08:34:32 +0100

    fx

--- app_sample/install.sh
+++ app_sample/install.sh
@@ -52,6 +52,12 @@ install_nginx() {
       }
       location @proxy {
         proxy_pass http://localhost:8080;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_http_version 1.1;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host:\$server_port;
         proxy_buffering off;
       }
     }
